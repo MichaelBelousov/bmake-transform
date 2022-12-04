2b2dbaeeed42db07f8af20e826bee81fac163bc2
Michael Belousov <mike.belousov@bentley.com>
2022-12-03 21:30:40 -0500

    initial simple grammar work

--- tree-sitter/grammar.js
+++ tree-sitter/grammar.js
@@ -1,6 +1,19 @@
 module.exports = grammar({
   name: "bmake",
   rules: {
-    source_file: ($) => "hello",
+    identifier: ($) => /[a-zA-Z_][a-zA-Z_0-9]*/,
+    source_file: ($) => repeat($.stmt),
+    stmt: ($) => choice($.var_assign, $.append_assign),
+    var_assign: ($) =>
+      seq(field("identifier", $.identifier), "=", field("value", $.value)),
+    append_assign: ($) =>
+      seq(field("identifier", $.identifier), "+", field("value", $.value)),
+    // NOTE: how the heck will this work?
+    restOfLine: ($) => /[^\n]*/,
   },
+  // prettier-ignore
+  extras: ($) => [
+    /#[^\n]*/, // comments
+    / \t\n/ // whitespace
+  ],
 });
