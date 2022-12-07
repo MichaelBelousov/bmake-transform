eb751f7dbb245444482780c38d5e264af781d966
Michael Belousov <mike.belousov@bentley.com>
2022-12-06 20:49:30 -0500

    some super simple ast generation

--- to_zig.py
+++ to_zig.py
@@ -15,26 +15,36 @@ __author__ = 'Michael Belousov'
 import tree_sitter
 
 def zigify(n: tree_sitter.Node):
-    to_zig[n.type]()
+    if n.type not in to_zig:
+        raise KeyError(f"unknown ast node type: '{n.type}'")
+    return to_zig[n.type](n)
 
 diagnostic_headers = {
     "%error": "Error occurred: {s}"
 }
 
+header = """\
+const std = @import("std");
+"""
+
 to_zig = {
-    'body': lambda n: sum(map(zigify, n.named_children)),
+    'body': lambda n: header + '\n'.join(map(zigify, n.children)),
     'source_file': lambda n: to_zig['body'](n),
-    'if': lambda n: f'if ({zigify(n.child_by_field_name("cond"))}) {{ {zigify(n.named_children[1])} }}',
-    'diagnostic': lambda n: f'std.debug.print({diagnostic_headers[n.children[0].type]}, .{{"{n.children[1].text}"}})',
+    'if': lambda n: f'if ({zigify(n.child_by_field_name("cond"))}) {{\n{zigify(n.named_children[1])}\n}}',
+    'diagnostic': lambda n: f'std.debug.print("{diagnostic_headers[n.children[0].type]}", .{{"{n.children[1].text.decode("utf8")}"}});\n',
+    'comment': lambda n: f'//{n.text.decode("utf8")[1:]}\n',
+    'is_defined': lambda n: to_zig['identifier'](n),
+    'identifier': lambda n: n.text.decode('utf8'),
+    # NOTE: need to figure out how to ignore these
+    'defined': lambda _: 'std.env.getVar',
+    '!': lambda _: '!',
+    '(': lambda _: '(',
+    ')': lambda _: ')',
 }
-
-
 
 tree_sitter.Language.build_library(
     'build/bmake.so',
-    [
-        'tree-sitter'
-    ]
+    ['tree-sitter']
 )
 
 BMAKE_LANG = tree_sitter.Language('build/bmake.so', 'bmake')
@@ -54,5 +64,7 @@ if __name__ == '__main__':
     args = argparser.parse_args()
     if isinstance(args.file, str):
         args.file = open(args.file, 'r')
-    src = bytes(args.file.read())
+    src = bytes(args.file.read(), 'utf8')
     ast = bmake_parser.parse(src)
+
+    print(zigify(ast.root_node))
